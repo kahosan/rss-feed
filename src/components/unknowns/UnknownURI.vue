5460bbc4111da3dee45f674dd8e30088f6dbfa20
kahosan <15116355657l@gmail.com>
2023-05-26 12:45:38 +0800

    fix: op-4

--- src/components/unknowns/UnknownURI.vue
+++ src/components/unknowns/UnknownURI.vue
@@ -6,7 +6,7 @@ defineProps<{ data: string[] }>()
   <div>
     <div text-5 color="[var(--c-h2-color)]">
       <div v-for="entry in data" :key="entry" mb-4 overflow-hidden>
-        <p op-4>
+        <p op-40>
           {{ entry.split(' | ').at(1) }}
         </p>
         <a target="_blank" :href="entry" block truncate transition hover:op-60>{{ entry.split(' | ').at(2) }}</a>
