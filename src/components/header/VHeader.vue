eec70fef4359721649e103e1e8cae57886f6efb1
kahosan <15116355657l@gmail.com>
2023-05-29 10:35:39 +0800

    chore: improve font size

--- src/components/header/VHeader.vue
+++ src/components/header/VHeader.vue
@@ -6,9 +6,11 @@ defineProps<{ changeSource: (s: DataSource) => void; active: (s: DataSource) => 
 <template>
   <div bg="#ebebeb" dark:bg="#272626" h-14 flex items-center p-4>
     <div mx-auto max-w-6xl flex="~ 1 justify-between items-center">
-      <h2>RSS Feed</h2>
+      <h2 text-5>
+        RSS Feed
+      </h2>
       <div flex="~ items-center">
-        <div children:pr-4 children:transition>
+        <div text-3.5 children:pr-4 children:transition>
           <button :class="active('search') ? 'op-60' : ''" @click="() => changeSource('search')">
             搜索
           </button>
