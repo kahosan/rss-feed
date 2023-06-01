cdfab2cc7caa6e74701d90d1e889c066f580be43
kahosan <15116355657l@gmail.com>
2023-06-01 12:15:31 +0800

    style: content center

--- src/components/data-view/DataView.vue
+++ src/components/data-view/DataView.vue
@@ -12,7 +12,7 @@ const date = (monthDay: string, published: string | Date) => {
 
 <template>
   <div v-for="entry in displayData" :key="entry.id" mb-8>
-    <div flex="~ justify-between" min-h-14>
+    <div flex="~ justify-between items-center" min-h-14>
       <div max-w-xl overflow-hidden>
         <a target="_blank" :href="entry.postLink" cursor-pointer text-5 transition hover:op-60>{{ entry.postTitle }}</a>
         <p max-w-140 truncate text-3.5 op-60>
