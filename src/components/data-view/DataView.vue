bdf135eb2b2e74513f8dbb53bd4ba82296e7d870
kahosan <15116355657l@gmail.com>
2023-06-05 16:33:16 +0800

    chore: cleanup code

--- src/components/data-view/DataView.vue
+++ src/components/data-view/DataView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts" generic="T extends RssEntry">
+<script setup lang="ts">
 import pangu from 'pangu'
 import { getYear } from 'date-fns'
-import { RssEntry } from '~/types/rss'
+import type { RssEntry } from '~/types/rss'
 
-const { dateType } = defineProps<{ dateType: 'year' | 'monthDay'; displayData: T[] }>()
+const { dateType } = defineProps<{ dateType: 'year' | 'monthDay'; displayData: RssEntry[] }>()
 
 const date = (monthDay: string, published: string | Date) => {
   const year = getYear(new Date(published))
@@ -15,14 +15,18 @@ const date = (monthDay: string, published: string | Date) => {
   <div v-for="entry in displayData" :key="entry.id" mb-8>
     <div flex="~ justify-between items-center" min-h-14>
       <div max-w-xl overflow-hidden>
-        <a target="_blank" :href="entry.postLink" cursor-pointer text-5 font-bold transition hover:op-60>{{ pangu.spacing(entry.postTitle) }}</a>
+        <a target="_blank" :href="entry.postLink" cursor-pointer text-5 font-bold transition hover:op-60>
+          {{ pangu.spacing(entry.postTitle) }}
+        </a>
         <p max-w-140 truncate text-3.5 op-60>
           {{ entry.description || '没有描述' }}
         </p>
       </div>
       <div ml-4 max-w-28 min-w-28 overflow-hidden text-4 text-secondary>
         <p>{{ date(entry.monthDay, entry.published) }}</p>
-        <a target="_blank" block cursor-pointer truncate transition hover:op-60 :href="entry.siteLink">{{ entry.siteTitle }}</a>
+        <a target="_blank" block cursor-pointer truncate transition hover:op-60 :href="entry.siteLink">
+          {{ entry.siteTitle }}
+        </a>
       </div>
     </div>
   </div>
