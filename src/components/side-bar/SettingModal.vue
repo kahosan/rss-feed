e637616c2b93d26928cd3e72c5231d5039261544
kahosan <15116355657l@gmail.com>
2023-09-20 14:34:20 +0800

    feat: support add group item

--- src/components/side-bar/SettingModal.vue
+++ src/components/side-bar/SettingModal.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { NCascader, NModal } from 'naive-ui'
+import { NCascader, NCheckbox, NCheckboxGroup, NModal, NSelect, NSpace } from 'naive-ui'
 import { storeToRefs } from 'pinia'
+import { useRssData } from '~/store/rss-data'
 import { useRssGroup } from '~/store/rss-group'
 
 const { toggleModal } = defineProps<{
@@ -9,9 +10,9 @@ const { toggleModal } = defineProps<{
 }>()
 
 const { rssGroup } = storeToRefs(useRssGroup())
-const { removeGroup, removeGroupItem } = useRssGroup()
+const { addGroupItem, removeGroup, removeGroupItem } = useRssGroup()
 
-const selectRssGroup = ref<string[]>([])
+const selectRssGroupForRemove = ref<string[]>([])
 
 const selectOptions = computed(() => rssGroup.value?.map(group => ({
   value: group.name,
@@ -22,14 +23,48 @@ const selectOptions = computed(() => rssGroup.value?.map(group => ({
   })),
 })))
 
+const { rssData } = storeToRefs(useRssData())
+
+const siteList = computed(() =>
+  [
+    ...new Set(rssData.value?.contents
+      .map(item => item.entries
+        .map(entry => entry)).flat(),
+    ),
+  ],
+)
+
+const siteTitleList = () => [...new Set(siteList.value.filter(item => !item.groupBy).map(item => item.siteTitle))]
+
+const groupItems = () => {
+  return siteTitleList().map(item => ({
+    label: item,
+    value: item,
+  }))
+}
+
+const selectRssGroupForAdd = ref<string[]>([])
+const groupSelect = ref<string[]>([])
+const groupList = computed(() => rssGroup.value?.map(group => group.name))
+
 const clean = () => {
-  selectRssGroup.value = []
+  selectRssGroupForRemove.value = []
 }
 
 const submit = () => {
-  if (selectRssGroup.value.length !== 0) {
-    selectRssGroup.value.forEach(itemTitle => removeGroupItem(itemTitle))
-    selectRssGroup.value.forEach(group => removeGroup(group))
+  if (selectRssGroupForRemove.value.length !== 0) {
+    selectRssGroupForRemove.value.forEach(itemTitle => removeGroupItem(itemTitle))
+    selectRssGroupForRemove.value.forEach(group => removeGroup(group))
+  }
+
+  if (selectRssGroupForAdd.value.length !== 0) {
+    const groupItems = selectRssGroupForAdd.value.map(value => ({
+      title: value,
+      link: siteList.value.find(item => item.siteTitle === value)?.siteLink ?? '',
+      description: siteList.value.find(item => item.siteTitle === value)?.description ?? '',
+    }))
+
+    addGroupItem(groupSelect.value[0], groupItems)
   }
 
   toggleModal()
@@ -54,16 +89,29 @@ const cancel = () => {
     :on-positive-click="submit"
     :on-negative-click="cancel"
   >
-    <template #header>
-      删除分组或分组内的内容
-    </template>
-    <div mt-4>
-      <NCascader
-        v-model:value="selectRssGroup"
-        :options="selectOptions"
-        multiple
-        check-strategy="parent"
-      />
-    </div>
+    <NSpace vertical size="medium">
+      <NSpace vertical>
+        删除分组或分组内的内容
+        <NCascader
+          v-model:value="selectRssGroupForRemove"
+          :options="selectOptions"
+          placeholder="请选择"
+          multiple
+          check-strategy="parent"
+        />
+      </NSpace>
+      <NSpace vertical>
+        为分组添加内容
+        <NCheckboxGroup v-model:value="groupSelect" :max="1">
+          <NCheckbox v-for="group in groupList" :key="group" :value="group" :label="group" />
+        </NCheckboxGroup>
+        <NSelect
+          v-model:value="selectRssGroupForAdd"
+          multiple
+          placeholder="选择站点"
+          :options="groupItems()"
+        />
+      </NSpace>
+    </NSpace>
   </NModal>
 </template>
