f004887eabdad08c5ad6def1dfb4428d03151797
kahosan <15116355657l@gmail.com>
2023-10-24 17:09:54 +0800

    style: feed card bg when hover

--- src/components/data-view/DataView.vue
+++ src/components/data-view/DataView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import pangu from 'pangu'
 import { formatISO } from 'date-fns'
-import { NCard, NTag } from 'naive-ui'
+import { NTag } from 'naive-ui'
 import { normalizationTitle } from '~/utils'
 
 import type { Feed } from '~/types/feeds'
@@ -21,10 +21,10 @@ function isHidden(item: Feed) {
 </script>
 
 <template>
-  <NCard v-for="feed of feeds" v-show="isHidden(feed)" :key="feed.id" embedded :bordered="false" mb-6 content-style="padding: 0.65rem;">
+  <div v-for="feed of feeds" v-show="isHidden(feed)" :key="feed.id" mb-8 p-4 transition hover:bg-zinc-50 dark:hover:bg-zinc-800>
     <div flex="~ justify-between items-center" min-h-14>
       <div max-w-2xl overflow-hidden>
-        <a target="_blank" :href="feed.postLink" cursor-pointer text-5 font-bold transition lt-md:text-4.5 hover:op-60>
+        <a target="_blank" :href="feed.postLink" cursor-pointer text-5 font-bold lt-md:text-4.5>
           {{ pangu.spacing(normalizationTitle(feed.postTitle)) }}
         </a>
         <div mt-2 flex items-center gap-2 md:hidden>
@@ -38,7 +38,7 @@ function isHidden(item: Feed) {
             </a>
           </div>
         </div>
-        <p mt-1 max-w-140 truncate text-3.5 op-60>
+        <p mt-2 max-w-140 line-clamp-2 text-3.5 op-60>
           {{ pangu.spacing(feed.postDescription) || '没有描述' }}
         </p>
       </div>
@@ -49,5 +49,5 @@ function isHidden(item: Feed) {
         </a>
       </div>
     </div>
-  </NCard>
+  </div>
 </template>
